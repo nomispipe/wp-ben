e721944cc884336f2b7401b761e2a01ec2abb98e
Simon Pfeiffer <simonpipe@Laptop-Simon.localdomain>
2023-03-06 15:08:23 +0100

    split index.php

--- public/wp-content/themes/ben/functions.php
+++ public/wp-content/themes/ben/functions.php
@@ -4,3 +4,5 @@ function add_theme_scripts_and_styles() {
 	wp_enqueue_style("style", get_stylesheet_uri());
 	wp_enqueue_script( 'main', get_template_directory_uri() . '/main.js', array(), 1, true);
 }
+
+add_action('wp_enqueue_scripts', 'add_theme_scripts_and_styles');
